2854f4b15ac0e9217837da015e391eadf247107a
dovt58 <dovt58@gmail.com>
2023-11-14 15:32:24 +0700

    add app controller

---
+++ Apis/WebAPI/Controllers/AppController.cs
@@ -0,0 +1,19 @@
+﻿using Application.Commons.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+// [Authorize(Policy = PolicyNames.AdminOnly)]
+public class AppController : ApiControllerBase
+{
+    private readonly Appsettings _appsettings;
+
+    public AppController(Appsettings appsettings)
+    {
+        _appsettings = appsettings;
+    }
+
+    [HttpGet("appsettings")]
+    public IActionResult GetAppsettings()
+        => Ok(_appsettings);
+}
